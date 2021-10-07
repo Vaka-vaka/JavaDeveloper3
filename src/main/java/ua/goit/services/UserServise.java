d12d6a311a4dc0755c5c3c4357828a4eaf146e73
Vaka-vaka <Waka2006@ukr.net>
2021-10-07 17:46:44 +0300

    Update UserServise.java
    methods

--- src/main/java/ua/goit/services/UserServise.java
+++ src/main/java/ua/goit/services/UserServise.java
@@ -1,12 +1,29 @@
 package ua.goit.services;
 
-import ua.goit.User;
-
-import java.util.HashMap;
-import java.util.Map;
+import ua.goit.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class UserServise {
 
     private Map<Integer, User> userMap = new HashMap<>();
+
+    //метод сохраняем юзера
+    public void putUser(User user) {
+        userMap.put(user.getId(), user);
+    }
+
+    //метод возврат юзера
+    public User getUser(Integer id) {
+        return userMap.get(id);
+    }
+    
+    //метод возвращае юзеров старше зазначеного року
+    public List<User> getUsersOlderThanSixteen(Integer compare) {
+        return userMap.values().stream()
+                   .filter(user -> user.getAge() > compare)
+                   .collect(Collectors.toList());
+    }
+
 
 }
