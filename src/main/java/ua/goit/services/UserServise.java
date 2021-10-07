6e60e53743f8ddad9c4a1b53e49896db58ca8a75
Vaka-vaka <Waka2006@ukr.net>
2021-10-07 17:03:49 +0300

    Create UserServise.java
    for storing users

---
+++ src/main/java/ua/goit/services/UserServise.java
@@ -0,0 +1,12 @@
+package ua.goit.services;
+
+import ua.goit.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserServise {
+
+    private Map<Integer, User> userMap = new HashMap<>();
+
+}
