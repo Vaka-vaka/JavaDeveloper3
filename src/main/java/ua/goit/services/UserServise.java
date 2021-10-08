dc59fa5e81da4d7e4dd89624a9db8714917aa386
Vaka-vaka <Waka2006@ukr.net>
2021-10-08 11:13:09 +0300

    Update UserServise.java
    methods getUsersBySex

--- src/main/java/ua/goit/services/UserServise.java
+++ src/main/java/ua/goit/services/UserServise.java
@@ -19,11 +19,17 @@ public class UserServise {
     }
     
     //метод возвращае юзеров старше зазначеного року
-    public List<User> getUsersOlderThanSixteen(Integer compare) {
+    public List<User> getUsersOlderThan(Integer compare) {
         return userMap.values().stream()
                    .filter(user -> user.getAge() > compare)
                    .collect(Collectors.toList());
     }
 
+    //метод возврата юзеров по полу
+    public List<User> getUsersBySex(Sex sex) {
+        return userMap.values().stream()
+                .filter(user -> user.getSex().equals(sex))
+                .collect(Collectors.toList());
+    }
 
 }
