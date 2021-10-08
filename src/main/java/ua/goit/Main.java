6d50fa813bb29fcac73d83fc31a48a26efb50ff4
Vaka-vaka <Waka2006@ukr.net>
2021-10-08 12:36:33 +0300

    Create Main.java
    created class Main

---
+++ src/main/java/ua/goit/Main.java
@@ -0,0 +1,25 @@
+/**
+ * JavaDeveloper3. Module 2. jUnitTest
+ *
+ *  @autor Valentin Mozul
+ *  @version of 08.10.2021
+ */
+
+package ua.goit;
+
+import ua.goit.services.UserServise;
+
+public class Main {
+
+    static UserServise servise = new UserServise();
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setAge(55);
+        user.setName("Aka");
+        user.setSex(Sex.MALE);
+        servise.putUser(user);
+    }
+
+
+}
