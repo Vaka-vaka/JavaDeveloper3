e5cb4d9feee0619731818cd5ba02b45fede9d472
Vaka-vaka <Waka2006@ukr.net>
2021-10-13 16:25:23 +0300

    Update Main.java

--- src/main/java/ua/goit/Main.java
+++ src/main/java/ua/goit/Main.java
@@ -17,10 +17,29 @@ public class Main {
         User user = new User();
         user.setId(1);
         user.setAge(55);
-        user.setName("Aka");
+        user.setName("Vaka");
         user.setSex(Sex.MALE);
         servise.putUser(user);
+
+        User user1 = new User();
+        user1.setId(2);
+        user1.setAge(70);
+        user1.setName("Ana");
+        user1.setSex(Sex.FEMALE);
+        servise.putUser(user1);
+
+        User user2 = new User();
+        user2.setId(3);
+        user2.setAge(20);
+        user2.setName("GAna");
+        user2.setSex(Sex.FEMALE);
+        servise.putUser(user2);
         System.out.println(servise.getAll());
+        System.out.println(servise.getUsersBySex(Sex.MALE));
+        System.out.println(servise.getUsersBySex(Sex.FEMALE));
+        System.out.println(servise.getUser(2));
+        System.out.println(servise.getUserJungleThan(35));
+        System.out.println(servise.getUsersOlderThan(45));
     }
 
 
