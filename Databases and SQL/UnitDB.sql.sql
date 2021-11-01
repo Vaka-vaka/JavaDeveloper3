dc3682656ace68a688221e40dc5500c54261b2cf
Vaka-vaka <Waka2006@ukr.net>
2021-11-01 17:08:04 +0200

    Update UnitDB.sql.sql

--- Databases and SQL/UnitDB.sql.sql
+++ Databases and SQL/UnitDB.sql.sql
@@ -16,3 +16,11 @@ create table groups (
 	id serial primary key,
 	name varchar(50) not null
 );
+
+create table group_to_user(
+	group_id integer not null, 
+	user_id integer not null, 
+	
+	constraint user_id_fk foreign key (user_id) references users (id),
+	constraint group_id_fk foreign key (group_id) references groups (id)
+);
