bf75223519ec15793040fc64bcdaeb663318b9c2
Vaka-vaka <Waka2006@ukr.net>
2021-11-01 16:55:50 +0200

    Create UnitDB.sql.sql

---
+++ Databases and SQL/UnitDB.sql.sql
@@ -0,0 +1,11 @@
+create table Users(
+	id serial primary key,
+	name varchar(50) not null,
+	description varchar(250) not null
+);
+
+create table Items(
+	users_id serial primary key,
+	name varchar(50) not null,
+	count integer not null
+); 
