ac9429885a164b2cff7afb4b2966d204bf8f8f82
Vaka-vaka <Waka2006@ukr.net>
2021-11-01 17:07:03 +0200

    Update UnitDB.sql.sql

--- Databases and SQL/UnitDB.sql.sql
+++ Databases and SQL/UnitDB.sql.sql
@@ -5,7 +5,10 @@ create table Users(
 );
 
 create table Items(
-	users_id serial primary key,
+	id serial primary key,
 	name varchar(50) not null,
-	count integer not null
+	count integer not null,
+	user_id integer not null,
+	constraint items_fk foreign key (user_id) references users(id)
 ); 
+
