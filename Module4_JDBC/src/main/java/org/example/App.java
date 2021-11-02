40b8ad10dae47a618970237be2bc0e5e8f065511
Vaka-vaka <Waka2006@ukr.net>
2021-11-02 16:48:48 +0200

    Update App.java

--- Module4_JDBC/src/main/java/org/example/App.java
+++ Module4_JDBC/src/main/java/org/example/App.java
@@ -1,13 +1,27 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ *  @autor Valentin Mozul
+ *  @version of 02.11.2021
+ */
+
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.example.dao.DbHelper;
+
+import java.sql.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws SQLException {
+
+        try(Connection connection = DbHelper.getConnection("localhost", 5432,
+                "postgres", "postgres", "A1S5nkO/J2*33Wu");
+            Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("select * from users");
+            resultSet.next();
+            System.out.println(resultSet.getString("name"));
+            resultSet.close();
+        }
     }
 }
