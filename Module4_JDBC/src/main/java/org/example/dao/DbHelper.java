ebe710651e3d98874e1ef3256c00ae7841a5da0f
Vaka-vaka <Waka2006@ukr.net>
2021-11-03 13:36:42 +0200

    Update DbHelper.java

--- Module4_JDBC/src/main/java/org/example/dao/DbHelper.java
+++ Module4_JDBC/src/main/java/org/example/dao/DbHelper.java
@@ -10,8 +10,19 @@ package org.example.dao;
 import java.sql.*;
 
 public class DbHelper {
+    private static Connection connection;
 
     private static String url = "jdbc:postgresql://%s:%s/%s?user=%s&password=%s";
+
+
+    public static void connectToDb() throws SQLException {
+        connection = getConnection("localhost", 5432,
+                "postgres", "postgres", "A1S5nkO/J2*33Wu");
+    }
+
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
 
     public static Connection getConnection(
             String host,
