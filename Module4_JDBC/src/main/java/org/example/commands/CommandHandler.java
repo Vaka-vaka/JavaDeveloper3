50021a8dab5517a2d6c6533de5f0efd3128ecdc9
Vaka-vaka <Waka2006@ukr.net>
2021-11-08 16:44:11 +0200

    Create CommandHandler.java

---
+++ Module4_JDBC/src/main/java/org/example/commands/CommandHandler.java
@@ -0,0 +1,30 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 08.11.2021
+ */
+
+package org.example.commands;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandHandler {
+
+    Map<String, Command> commandMap = new HashMap<>();
+
+    public CommandHandler() {
+        commandMap.put("users", new CommandUsers());
+    }
+
+    public void handleCommand(String params) {
+        int firstSpace = params.indexOf(" ");
+        if (firstSpace > -1) {
+            Command command = commandMap
+                    .get(params.substring(0, firstSpace));
+            command.handle(params.substring(firstSpace + 1));
+        }
+    }
+
+}
