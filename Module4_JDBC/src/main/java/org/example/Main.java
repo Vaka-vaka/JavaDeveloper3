01641bdda3d1b0c1b708e2d3dd95d5d3036db0a3
Vaka-vaka <Waka2006@ukr.net>
2021-11-08 12:06:05 +0200

    Update Main.java

--- Module4_JDBC/src/main/java/org/example/Main.java
+++ Module4_JDBC/src/main/java/org/example/Main.java
@@ -7,86 +7,36 @@
 
 package org.example;
 
-import org.example.dao.DbHelper;
-import org.example.model.User;
-
-import java.sql.*;
+import org.example.dao.Dao;
+import org.example.dao.UserDao;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+        Dao<?> userDao = new UserDao();
 
-        User user = getUser(4L);
-        if (user != null) {
-            System.out.println(user);
-            user.setName("Ork");
-            user.setDescription("grin");
-            //  createUser(user);
-      //        deleteUser(user);
-            updateUser(user);
-            user = getUser(4L);
-            System.out.println(user);
-        }
+        System.out.println("Start application");
+        List<?> all = userDao.getAll();
+
+        System.out.println(all);
+
+
+        userDao.get(10L)
+                .ifPresent(System.out :: println);
+        System.out.println("And application");
+
+//        User user = getUser(4L);
+//        if (user != null) {
+//            System.out.println(user);
+//            user.setName("Ork");
+//            user.setDescription("grin");
+//            //  createUser(user);
+//      //        deleteUser(user);
+//            updateUser(user);
+//            user = getUser(4L);
+//            System.out.println(user);
+//        }
          //   getAllUsers();
     }
 
-    private static void deleteUser(User user) {
-        String sql = "delete from users where id = ?";
-        int count = DbHelper.executeWithPreparedStatement(sql, ps -> {
-            ps.setLong(1, user.getId());
-        });
-        System.out.println("Deleted " + count + " records");
-    }
-
-    private static void createUser(User user) {
-        String sql = "insert into users(name, description) values (?, ?)";
-        int count = DbHelper.executeWithPreparedStatement(sql, ps -> {
-            ps.setString(1, user.getName());
-            ps.setString(2, user.getDescription());
-        });
-        System.out.println("Created " + count + " records");
-    }
-
-
-    private static void updateUser(User user) {
-        String sql = "update users set name = ?, description = ? where id = ?";
-        int count = DbHelper.executeWithPreparedStatement(sql, ps -> {
-            ps.setString(1, user.getName());
-            ps.setString(2, user.getDescription());
-            ps.setLong(3, user.getId());
-        });
-        System.out.println("Updated " + count + " records");
-    }
-
-    private static void getAllUsers() throws SQLException { //непаше ??
-        List<User> users = new ArrayList<>();
-        ResultSet resultSet = DbHelper.getWithPreparedStatement(
-                "select * from users", ps -> {
-                });
-        while (resultSet.next()) {
-            users.add(convertUser(resultSet));
-        }
-        System.out.println(users);
-    }
-
-    private static User getUser(Long id) throws SQLException {
-        ResultSet resultSet = DbHelper.getWithPreparedStatement(
-                "select * from users where id = ?", ps -> {
-                    ps.setLong(1, id);
-                });
-        if (resultSet.next()) {
-            System.out.println("Record was selected");
-            return convertUser(resultSet);
-        } else {
-            return null;
-        }
-    }
-
-    public static User convertUser(ResultSet resultSet) throws SQLException {
-        User user = new User();
-        user.setId(resultSet.getLong("id"));
-        user.setName(resultSet.getString("name"));
-        user.setDescription(resultSet.getString("description"));
-        return user;
-    }
 }
