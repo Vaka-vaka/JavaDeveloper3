202a5aae53fd77c68383e1c3f74a26118e69ddc6
Vaka-vaka <Waka2006@ukr.net>
2021-11-10 11:11:57 +0200

    Create ItemService.java

---
+++ Module4_JDBC/src/main/java/org/example/service/ItemService.java
@@ -0,0 +1,15 @@
+/**
+ * JavaDeveloper3. Module 4. JDBC
+ *
+ * @autor Valentin Mozul
+ * @version of 10.11.2021
+ */
+
+
+package org.example.service;
+
+public class ItemService {
+
+    private ItemDao dao = new ItemDao();
+
+}
