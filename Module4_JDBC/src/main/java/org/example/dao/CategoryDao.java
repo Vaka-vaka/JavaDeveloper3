e2b2390f2581495493ca310f07761885b9ebceb4
Vaka-vaka <Waka2006@ukr.net>
2021-11-12 14:47:11 +0200

    Update CategoryDao.java

--- Module4_JDBC/src/main/java/org/example/dao/CategoryDao.java
+++ Module4_JDBC/src/main/java/org/example/dao/CategoryDao.java
@@ -44,6 +44,13 @@ public class CategoryDao extends AbstractDao<Category>{
 
     @Override
     public void update(Category entity) {
-
+        String sql = "update category set name = ?, description = ?, parent_id = ? where id = ?";
+        int count = DbHelper.executeWithPreparedStatement(sql, ps -> {
+            ps.setString(1, entity.getName());
+            ps.setString(2, entity.getDescription());
+            ps.setLong(3, entity.getParentId());
+            ps.setLong(4, entity.getId());
+        });
+        System.out.println("Updated " + count + " records");
     }
 }
