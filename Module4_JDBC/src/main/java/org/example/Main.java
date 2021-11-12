a38292aee2ac5c445f9706f5e3842d1eaa693819
Vaka-vaka <Waka2006@ukr.net>
2021-11-12 14:47:20 +0200

    Update Main.java

--- Module4_JDBC/src/main/java/org/example/Main.java
+++ Module4_JDBC/src/main/java/org/example/Main.java
@@ -7,25 +7,13 @@
 
 package org.example;
 
-import org.example.config.DbMigration;
 import org.example.console.CommandHandler;
 import java.util.*;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 public class Main {
 
-      //  private static final Logger LOGGER = LogManager.getLogger(Main.class);
-
         public static void main (String[]args){
-//            LOGGER.debug("Start application");
-//            DbMigration.migrate();
-//
-//            runMainApp();
-//            LOGGER.info("END application");
-//        }
-//
-//        public static void runMainApp () {
+
             System.out.println("Start application");
             CommandHandler commandHandler = new CommandHandler();
             Scanner scanner = new Scanner(System.in);
