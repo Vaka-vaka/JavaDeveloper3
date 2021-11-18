1d363e8eb43eac68cf0c34a217b6974721e03fcc
Vaka-vaka <Waka2006@ukr.net>
2021-11-18 10:43:38 +0200

    Create WebServer.java

---
+++ module 5 HTTP/src/main/java/ua/goit/server/WebServer.java
@@ -0,0 +1,31 @@
+package ua.goit.server;
+
+import com.sun.net.httpserver.HttpServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class WebServer {
+
+    private static final Logger LOGGER = LogManager.getLogger(WebServer.class);
+
+    public void start() {
+        try {
+            HttpServer server = HttpServer.create(
+                    new InetSocketAddress("localhost", 80), 0);
+
+            server.createContext("/", new IndexHandler());
+            server.start();
+        } catch (IOException e) {
+           LOGGER.error("Problem with starting web server", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        WebServer server = new WebServer();
+        server.start();
+    }
+
+}
