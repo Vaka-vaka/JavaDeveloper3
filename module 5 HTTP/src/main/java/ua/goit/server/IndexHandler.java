98ed97d6b762ef6c50d5b528a6d7cfdda8cb99f7
Vaka-vaka <Waka2006@ukr.net>
2021-11-18 17:19:45 +0200

    Update IndexHandler.java

--- module 5 HTTP/src/main/java/ua/goit/server/IndexHandler.java
+++ module 5 HTTP/src/main/java/ua/goit/server/IndexHandler.java
@@ -1,13 +1,25 @@
+/**
+ * Module 5. HTTP
+ *
+ * @autor Valentin Mozul
+ * @version of 18.11.2021
+ */
+
 package ua.goit.server;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class IndexHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        System.out.println("Hello");
+
+        exchange.sendResponseHeaders(500, 0);
+        OutputStream responseBody = exchange.getResponseBody();
+        responseBody.write("Hello".getBytes());
+        responseBody.close();
     }
 }
