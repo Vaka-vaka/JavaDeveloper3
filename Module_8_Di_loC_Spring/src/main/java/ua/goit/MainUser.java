242567a815e527b0a02ceb1c026d303a8ad4decb
Vaka-vaka <Waka2006@ukr.net>
2022-01-26 16:04:21 +0200

    service 1

--- Module_8_Di_loC_Spring/src/main/java/ua/goit/MainUser.java
+++ Module_8_Di_loC_Spring/src/main/java/ua/goit/MainUser.java
@@ -4,13 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ua.goit.model.Developers;
-import ua.goit.model.Skills;
-import ua.goit.repositories.SkillsRepository;
-import ua.goit.repositories.UserRepository;
-
-import java.util.List;
-import java.util.Optional;
+import ua.goit.service.UserService;
 
 @ComponentScan
 @EnableJpaRepositories
@@ -19,29 +13,11 @@ public class MainUser {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(MainUser.class);
 
-        UserRepository repository = context.getBean(UserRepository.class);
-        SkillsRepository skillsRepository = context.getBean(SkillsRepository.class);
-        Optional<Developers> developers = repository.findById(1L);
-        if(developers.isPresent()) {
-            Developers developers1 = developers.get();
-            System.out.println("Name - " + developers1.getName_());
-            developers1.setName_("Valera");
-            repository.save(developers1);
-        }
+        UserService service = context.getBean(UserService.class);
+        service.printAllUsers();
+        service.printGetUsers();
 
-        Optional<Developers> developers2 = repository.findById(1L);
-        if(developers.isPresent()) {
-            Developers developers1 = developers.get();
-            System.out.println("Name after - " + developers1.getName_());
-
-//        Optional<User> byId = repository.findById(1L);
-//        if(byId.isPresent()) {
-//            System.out.println(byId.get());
-//        }
-//        List<Developers> users = repository.findAll();
-//        users.forEach(System.out::println);
 
         }
     }
 
-}
--- Module_8_Di_loC_Spring/src/main/java/ua/goit/service/UserService.java
+++ Module_8_Di_loC_Spring/src/main/java/ua/goit/service/UserService.java
@@ -2,7 +2,10 @@ package ua.goit.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ua.goit.model.Developers;
 import ua.goit.repositories.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,6 +15,15 @@ public class UserService {
 
     public void printAllUsers() {
         repository.findAll().forEach(System.out::println);
+    }
+
+    public void printGetUsers() {
+        Optional<Developers> byId = repository.findById(4L);
+        if(byId.isPresent()) {
+            Developers developers = byId.get();
+            System.out.println(developers);
+        }
+
     }
 
 }
